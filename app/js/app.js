e402d1b694c3be7bea149d5f7692f86e886c2cd5
Sergey Valitov <serejatoje@gmail.com>
2020-12-16 20:58:23 +0700

    !

--- app/js/app.js
+++ app/js/app.js
@@ -1,28 +1,38 @@
 $(document).ready(function () {
 
-    let script = document.createElement("script");
-    script.type = "text/javascript";
-    script.src = "app/js/leaflet.min.js";
+    setTimeout(function() {
 
-    document.getElementsByTagName("body")[0].appendChild(script)
+        let script = document.createElement("script");
+        script.type = "text/javascript";
+        script.src = "app/js/leaflet.min.js";
 
-    script.onload = function() {
-        let map;
-        if ($(window).width() <= 980) {
-            map = L.map('map', {attributionControl: false, scrollWheelZoom: false}).setView([55.342998, 86.090009], 16);
-        } else {
-            map = L.map('map', {attributionControl: false, scrollWheelZoom: false}).setView([55.342698, 86.089009], 18);
-        }
+        document.getElementsByTagName("body")[0].appendChild(script)
 
-        L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png').addTo(map);
-        let pin = L.icon({
-            iconUrl: 'app/images/sprite.svg#pin',
-            iconSize: [76, 80],
-            iconAnchor: [38, 80],
-        });
+        script.onload = function () {
+            let map;
+            if ($(window).width() <= 980) {
+                map = L.map('map', {
+                    attributionControl: false,
+                    scrollWheelZoom: false
+                }).setView([55.342998, 86.090009], 16);
+            } else {
+                map = L.map('map', {
+                    attributionControl: false,
+                    scrollWheelZoom: false
+                }).setView([55.342698, 86.089009], 18);
+            }
 
-        L.marker([55.342698, 86.090009], {icon: pin}).addTo(map);
-    };
+            L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png').addTo(map);
+            let pin = L.icon({
+                iconUrl: 'app/images/sprite.svg#pin',
+                iconSize: [76, 80],
+                iconAnchor: [38, 80],
+            });
+
+            L.marker([55.342698, 86.090009], {icon: pin}).addTo(map);
+        };
+
+    }, 1200);
 
     $('.btn-burger')
         .click(function() {
